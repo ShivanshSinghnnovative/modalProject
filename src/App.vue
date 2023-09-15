928d6af8478d2882f244dfe703326446c872bbed
ShivanshSinghnnovative <shivansh.singh@innovative.in>
2023-09-15 16:09:30 +0530

    codexs

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+  <div class="contentBox">
+    <ModalS nameTitle="Sign up for the Giveaway!" , name="Grab your ninja swag for half Price">
+      <template #links>
+        <a href="">Google</a>
+        <a href="">Facebook</a>
+      </template>
+    </ModalS>
+    <input ref="myInput" type="text" id="input" />
+    <button @click="focusInput()" id="focusButton">Focus</button>
+
+
+  </div>
+</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+  
 
+
+<script>
+import ModalS from './components/ModalS.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ModalS
+  },
+  methods: {
+    focusInput() {
+      this.$refs.myInput.focus();
+    }
   }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style >
+.container{
+  position: relative;
+  width : 100%;
+  height: 100vh;
+}
+button {
+  background: #1d3027;
+  color: white;
+  border-radius: .2rem;
+  cursor: pointer;
+
+}
+
+.contentBox {
+  border: 2px solid black;
+  background-color: rgb(40, 82, 82);
+  box-shadow: 5px 7px #dfdede;
+  margin-left: auto;
+  margin-right: auto;
+  width: fit-content;
+  margin-top: 10%;
+  padding: 5rem;
+  gap: 1rem;
+
+}
+
+#input {
+  display: block;
+  height: .8rem;
+  padding: .5rem;
+  width: 12rem;
+}
+
+#focusButton {
+  padding: .5rem;
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 1rem;
 }
 </style>
---
+++ src/components/ModalS.vue
@@ -0,0 +1,124 @@
+<template>
+    <div id="hideBox">
+        <div class="MainDiv" v-on:click="hideModel()">
+        </div>
+
+        <div class="centerDiv">
+            <h2 class="modalTitle">{{ nameTitle }}</h2>
+            <p class="modalContent">{{ name }}</p>
+            <slot name="links" class="slot"></slot>
+        </div>
+    </div>
+    <button id="modelButton" v-on:click="openData()">ShowModal</button>
+</template>
+<script>
+export default {
+    name: 'ModalS',
+    props: ['nameTitle', 'name'],
+    methods: {
+        openData() {
+            document.getElementById('hideBox').style.display = "block";
+            document.getElementById('modelButton').style.display = "none";
+        },
+        hideModel() {
+            document.getElementById('hideBox').style.display = "none";
+            document.getElementById('modelButton').style.display = "flex";
+        }
+    }
+
+}
+
+
+</script>
+<style  >
+* {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    overflow-x: hidden;
+    overflow-y: hidden;
+}
+
+#hideBox {
+    display: none;
+    margin: 0;
+    padding: 0;
+   
+}
+
+.MainDiv {
+   height: 100vh;
+   width: 100vw;
+    z-index: 10;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    background-color: rgb(0, 0, 0, 0.2);
+
+    
+}
+
+.slot {
+    text-align: center;
+    padding: 5px;
+}
+
+a {
+    border: 1px solid black;
+    text-decoration: none !important;
+    padding: 5px;
+    display: inline;
+    border-radius: 2px;
+    padding: 0.5rem 1rem;
+    margin-left: auto;
+    margin-right: auto;
+    width: fit-content;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: white;
+    background-color: grey;
+}
+
+.centerDiv {
+    width: fit-content;
+
+  
+    margin-right: auto;
+    position: absolute;
+    z-index: 10;
+    border-radius: 0.5rem;
+
+    left: 35%;
+    top: 30%;
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    background-color: rgb(186, 46, 46);
+
+}
+
+.modalTitle {
+    color: white;
+    font-weight: 600;
+    font-size: 33px;
+    padding: 1.4rem 4rem;
+}
+
+.modalContent {
+    color: white;
+    text-align: center;
+    padding-bottom: 1.3rem;
+}
+
+#modelButton {
+    padding: .5rem;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 1rem;
+}
+</style>
